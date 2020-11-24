d3f3ea91ea64f88afc320278bef8effe7141a14c
Huw Wynn-Jones <huw@huwwynnjones.com>
2020-11-24 23:23:04 +0000

    Tested some variations for increasing the array length and repetitions

--- src/lib.rs
+++ src/lib.rs
@@ -117,4 +117,10 @@ mod tests {
     fn test_increase_array_length_and_repetitions() {
         assert_eq!(increase_array_length_and_repetitions(0, 0), (5, 2));
     }
+
+    #[test]
+    fn test_various_increase_array_length_and_repetitions() {
+        assert_eq!(increase_array_length_and_repetitions(5, 2), (15, 4));
+        assert_eq!(increase_array_length_and_repetitions(15, 4), (35, 8));
+    }
 }
